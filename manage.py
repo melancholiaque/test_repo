464d5837cfbf64d3abd9fad8eacbe7feae2945c2
melancholiaque <melancholique655312@gmail.com>
2019-02-11 18:39:42 +0200

    123

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from app import app
-from os import environ, argv
+from os import environ
+from sys import argv
 
+print(argv)
 if argv[1] == 'run': 
     app.run(host='0.0.0.0', port=environ['PORT'])
